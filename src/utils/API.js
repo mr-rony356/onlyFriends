025ca527d5df2fa0cc9c4fe079187b317786fdbf
Omor Faruk Rony <99247805+mr-rony356@users.noreply.github.com>
2024-09-14 14:38:57 +0600

    Update API.js
    railway domain added

--- src/utils/API.js
+++ src/utils/API.js
@@ -5,7 +5,7 @@ import Cookies from "js-cookie";
  */
 export const API_ADDRESS =
   process.env.NEXT_PUBLIC_ENVIRONMENT === "production"
-    ? "https://onlyfriend.ch/"
+    ? "https://onlyfriends-production-84ef.up.railway.app"
     : "http://localhost:3000/";
 
 /**
