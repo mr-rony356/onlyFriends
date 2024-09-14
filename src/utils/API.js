3c467fb901e2e2df69d09e416c489c808e346b4d
Omor Faruk Rony <omorfarukrony2@gmail.com>
2024-09-14 23:40:47 +0600

    pritter fix

--- src/utils/API.js
+++ src/utils/API.js
@@ -5,8 +5,8 @@ import Cookies from "js-cookie";
  */
 export const API_ADDRESS =
   process.env.NEXT_PUBLIC_ENVIRONMENT === "production"
-  ? "https://onlyfriend.ch/"
-  : "http://localhost:3000/";
+    ? "https://onlyfriend.ch/"
+    : "http://localhost:3000/";
 
 /**
  * FETCH REQUESTS CONTROLLER
