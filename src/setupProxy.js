2e0c8a9b8f8b721f279c173e1ea1f35fba249db7
Omor Faruk Rony <omorfarukrony2@gmail.com>
2024-09-14 14:51:04 +0600

    proxy update

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -6,7 +6,7 @@ module.exports = function (app) {
     createProxyMiddleware({
       target:
         process.env.NEXT_PUBLIC_ENVIRONMENT === "production"
-          ? "https://onlyfriend.ch/"
+          ? "https://onlyfriends-production-84ef.up.railway.app"
           : "http://localhost:3000/",
       changeOrigin: true,
     }),
