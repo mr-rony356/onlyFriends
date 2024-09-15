075a2f23cc18f83c5903adb745daa32cbaedfd03
Omor Faruk Rony <omorfarukrony2@gmail.com>
2024-09-15 10:41:52 +0600

    api to only friend

--- src/utils/API.js
+++ src/utils/API.js
@@ -5,7 +5,7 @@ import Cookies from "js-cookie";
  */
 export const API_ADDRESS =
   process.env.NEXT_PUBLIC_ENVIRONMENT === "production"
-    ? "onlyfriends-production.up.railway.app/"
+    ? "https://onlyfriend.ch/"
     : "http://localhost:3000/";
 
 /**
