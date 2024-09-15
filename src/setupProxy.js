66d0dc1ccc9170135c1cecca40a948001e5c1f06
Omor Faruk Rony <omorfarukrony2@gmail.com>
2024-09-15 09:55:53 +0600

    api to main

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -6,7 +6,7 @@ module.exports = function (app) {
     createProxyMiddleware({
       target:
         process.env.NEXT_PUBLIC_ENVIRONMENT === "production"
-          ? "https://www.onlyfriend.ch/"
+          ? "https://onlyfriend.ch/"
           : "http://localhost:3000/",
       changeOrigin: true,
     }),
--- src/utils/API.js
+++ src/utils/API.js
@@ -5,7 +5,7 @@ import Cookies from "js-cookie";
  */
 export const API_ADDRESS =
   process.env.NEXT_PUBLIC_ENVIRONMENT === "production"
-    ? "https://www.onlyfriend.ch/"
+    ? "https://onlyfriend.ch/"
     : "http://localhost:3000/";
 
 /**
